3b6d9038e1abf6d7d4af112520387b535a1dfa76
Kier von Konigslow <kvonkonigslow@gmail.com>
2020-04-20 12:58:11 -0400

    Improved test for insert async, added one for overload

--- CarbonitePersist.UnitTests/TestCarboniteDatabase.cs
+++ CarbonitePersist.UnitTests/TestCarboniteDatabase.cs
@@ -1,10 +1,30 @@
 using System;
+using System.Collections.Generic;
 using System.IO;
 using System.Threading.Tasks;
 using Xunit;
 
 namespace CarbonitePersist.UnitTest
 {
+    public class Customer
+    {
+        public int Id { get; set; }
+        public string Firstname { get; set; }
+        public string Lastname { get; set; }
+        public string Email { get; set; }
+        public string Phone { get; set; }
+        public DateTime CustomerSince { get; set; }
+    }
+
+    public class Order
+    {
+        public Guid Id { get; set; }
+        public double Subtotal { get; set; }
+        public double Total { get; set; }
+        public bool IsFilled { get; set; }
+        public Customer Customer { get; set; }
+    }
+
     public class TestCarboniteDatabase
     {
         [Fact]
@@ -12,17 +32,80 @@ namespace CarbonitePersist.UnitTest
         {
             var database = Guid.NewGuid().ToString();
             var path = Path.Combine(Path.GetTempPath(), database);
-            var db = new CarboniteTool($"Database={path}");
-            var col = db.GetCollection<MyEntity>();
+            var db = new CarboniteTool($"Path={path}");
+            var col = db.GetCollection<Order>();
 
-            var entity = new MyEntity
+            var customer = new Customer
             {
-                Id = 6,
-                Name = "First",
-                IsCreated = true,
+                Id = 2,
+                Firstname = "Bill",
+                Lastname = "Masterson",
+                Email = "bill.masterson@email.com",
+                Phone = "555-1234",
+                CustomerSince = DateTime.Parse("2008-11-01"),
             };
 
-            await col.InsertAsync(entity);
+            var order = new Order
+            {
+                Id = new Guid("16db1209-ac9c-472f-bf76-5ba4dcecf2bd"),
+                Subtotal = 240.99,
+                Total = 258.25,
+                IsFilled = false,
+                Customer = customer,
+            };
+
+            await col.InsertAsync(order);
+
+            Directory.Delete(path, true);
+        }
+
+        [Fact]
+        public async Task TestInsertOverloadAsync()
+        {
+            var database = Guid.NewGuid().ToString();
+            var path = Path.Combine(Path.GetTempPath(), database);
+            var db = new CarboniteTool($"Path={path}");
+            var col = db.GetCollection<Order>();
+
+            var customerA = new Customer
+            {
+                Id = 2,
+                Firstname = "Bill",
+                Lastname = "Masterson",
+                Email = "bill.masterson@email.com",
+                Phone = "555-1234",
+                CustomerSince = DateTime.Parse("2008-11-01"),
+            };
+
+            var customerB = new Customer
+            {
+                Id = 3,
+                Firstname = "Jill",
+                Lastname = "James",
+                Email = "jjames@domain.com",
+                Phone = "555-9456",
+                CustomerSince = DateTime.Parse("2012-03-11"),
+            };
+
+            var orderA = new Order
+            {
+                Id = new Guid("06a1bac6-b534-421d-a130-1441fe0ef5c6"),
+                Subtotal = 55.23,
+                Total = 60.91,
+                IsFilled = true,
+                Customer = customerA,
+            };
+
+            var orderB = new Order
+            {
+                Id = new Guid("16db1209-ac9c-472f-bf76-5ba4dcecf2bd"),
+                Subtotal = 240.99,
+                Total = 258.25,
+                IsFilled = false,
+                Customer = customerB,
+            };
+
+            await col.InsertAsync(new List<Order> { orderA, orderB });
 
             Directory.Delete(path, true);
         }
